31f90482906a9cdd797e51884d480ab8a2a0c33c
Christhopher Lion <christhopherleao@icloud.com>
2021-07-21 15:18:31 +0200

    charkad
    Signed-off-by: Christhopher Lion <christhopherleao@icloud.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import { ChakraProvider as ThemeProvider, extendTheme } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { theme as defaultTheme } from "theme/theme";
 
 import { PollContext } from "services";
-import { theme as defaultTheme } from "services/styles/theme/theme";
 
 import { QuestionStack, PageContainer } from "components";
 
 const theme = extendTheme(defaultTheme);
 
 function App() {
-  // const theme = useTheme();theme
-
   return (
     <ThemeProvider theme={theme}>
       <PageContainer>
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,2 @@
-// Types
-export * from "./styles/theme/theme.types";
-
 // Hooks
-export { useStyles } from "./styles/useStyles";
 export { usePoll, PollContext } from "./usePoll/usePoll";
-export { useTheme } from "./styles/theme/useTheme";
