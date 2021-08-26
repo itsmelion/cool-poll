217b3783b580cc8953ff519591475ee644622313
Christhopher Lion <christhopherleao@icloud.com>
2021-08-26 13:56:15 +0200

    fix export
    Signed-off-by: Christhopher Lion <christhopherleao@icloud.com>

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 export * from "./src/types";
 export { Quiz } from "./src/Quiz";
-export { QuizComponent } from "./src/components";
+export { QuizComponent } from "./src/components/QuizComponent";
 export { theme } from "./src/theme/theme";
 export { ThemeProvider } from "./src/theme/ThemeProvider";
 export { usePoll, usePollActions, PollProvider } from "./src/services";
